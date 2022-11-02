d382add1f5b1d06b87c668f13b46cd2d1aa11f01
Ivan1337 <79834829+Ivan881914@users.noreply.github.com>
2022-11-02 22:44:45 +0300

    Add files via upload

---
+++ Новобранцы/Model/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recruits.Model;
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    //[Required]
+    public string? RecruitName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? RecruitLink { get; set; }
+    public string? RecruitJobTitle { get; set; }
+    public string? FirstInterview { get; set; }
+    public string? HRName { get; set; }
+    public string? HRJobTitle { get; set; }
+    public string? TestRealizationTime { get; set; }
+    public string? TestСompletionTime { get; set; }
+    public string? Comment { get; set; }
+
+
+}
+

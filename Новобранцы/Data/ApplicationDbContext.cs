d4269fd392894fd88c4ee1684a5aedcaed22de7d
Ivan1337 <79834829+Ivan881914@users.noreply.github.com>
2022-11-02 22:45:13 +0300

    Add files via upload

---
+++ Новобранцы/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recruits.Model;
+
+namespace Recruits.Model;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Category> Category { get; set; }
+}
+// add-migration AddCategoryToDb
+// update-database
